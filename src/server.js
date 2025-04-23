b9ffbf272e176d9e1375bbc1b964295d923a1b03
Rachit123kumar <hellobittukumar12@gmail.com>
2025-04-23 07:22:13 +0530

    fixing error

--- src/server.js
+++ src/server.js
@@ -19,6 +19,13 @@ import { URL } from "node:url";
 dotenv.config() 
 const app = express();
 app.use(express.json())
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"]
+  }));
 const httpServer = app.listen(process.env.PORT)
 const wss = new WebSocketServer({ server: httpServer })
 const chatManager = new ChatManager()
@@ -28,12 +35,7 @@ const chatManager = new ChatManager()
 //     credentials: false,
 //   }));
 // app.use(cors())
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    allowedHeaders: ["Authorization", "Content-Type"]
-  }));
+
   
 
 
